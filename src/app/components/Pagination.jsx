55302e3766451e8a923394ae1fecedea637092a7
Lubomir80 <Kutsiyl@gmail.com>
2024-03-18 19:27:32 +0100

    footer

--- src/app/components/Pagination.jsx
+++ src/app/components/Pagination.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Pagination() {
    return (
-      <div>Pagination</div>
+      <div className='flex justify-between mt-10'>
+         <button className='w-24 border-0 p-4 bg-red-500 text-white cursor-pointer'>Previus</button>
+         <button className='w-24 border-0 p-4 bg-red-500 text-white cursor-pointer'>Next</button>
+      </div>
    )
 }
 
