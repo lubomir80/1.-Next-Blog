c3124118018f531ef3830405ecc26adf5303e057
Lubomir <100048089+lubomir80@users.noreply.github.com>
2024-04-04 00:05:34 +0200

    LoginPage

---
+++ src/app/login/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function LoginPage() {
+   return (
+      <div className='flex items-center justify-center mt-12'>
+         <div className='px-10 py-14 phone:p-20 tablet:p-32 bg-slate-200 dark:bg-slate-600  flex flex-col justify-center items-center gap-12 rounded-md'>
+            <button className='w-[190px] px-4 py-2 tablet:px-4  cursor-pointer  bg-red-400 rounded-md text-white text-center'>Sign in with Google</button>
+            <button className='w-[190px] px-4 py-2 tablet:px-4  cursor-pointer  bg-slate-800 rounded-md text-white text-center'>Sign in with Github</button>
+            <button className='w-[190px] px-4 py-2 tablet:px-4  cursor-pointer  bg-blue-600 rounded-md text-white text-center'>Sign in with Facebook</button>
+         </div>
+      </div>
+   )
+}
+
+export default LoginPage
