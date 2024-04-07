0b7fffedbec86fd1be042d78dd31b0646e5d3cc9
Lubomir <100048089+lubomir80@users.noreply.github.com>
2024-04-07 14:53:56 +0200

    Revert "writePage input"
    This reverts commit a8f158394f5045894ee629a74147028001738f45.

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+"use client"
+import Image from 'next/image';
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 function WritePage() {
+   const [open, setOpen] = useState(false);
+   const [value, setValue] = useState(false);
+
    return (
-      <div>WritePage</div>
+      <div>
+
+      </div>
    )
 }
 
